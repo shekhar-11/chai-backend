85deec56b6b8ac3fb768f2e39f5fe2e55a4f11cc
shekhar-11 <rajshekhar111201@gmail.com>
2025-02-05 12:35:39 +0530

    Segregated the files for db connection and dotenv setup

--- src/index.js
+++ src/index.js
@@ -1,20 +1,79 @@
+import dotenv from "dotenv"
+dotenv.config({
+  path:"./.env"
+})
+
 import express from "express";
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// import { DB_NAME } from "./constants";
+import connectDb from "./db/db.js";
 const app = express();
 
+connectDb();
 
 
-;(async ()=>{
-  try{
-    await mongoose.connect(`${process.env.MONGODB_URI}\${DB_NAME}`)
-    app.on(err,(err)=>{
-      console.log("Error listening to the db by express ",err);
-      throw err
-    })
-  }
-  catch(err)
-  {
-    console.log("Error connecting to db: ",err );
-  }
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// require("dotenv").config({
+// path:"./env"});
+// ;(async ()=>{
+//   try{
+//     await mongoose.connect(`${process.env.MONGODB_URI}\${DB_NAME}`)
+//     app.on(err,(err)=>{
+//       console.log("Error listening to the db by express ",err);
+//       throw err
+//     })
+//   }
+//   catch(err)
+//   {
+//     console.log("Error connecting to db: ",err );
+//   }
+// })
